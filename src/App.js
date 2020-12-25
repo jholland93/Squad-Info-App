2f9bc9989b63f255ba6e1cf98a3afdcf09bf30ed
jholland93 <g00363203@gmit.ie>
2020-12-25 01:46:07 +0000

    set up project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React from "react";
+import { Component } from "react";
+import "./App.css";
+import { Content } from "./components/content";
+import { Footer } from "./components/footer";
+import { Players } from "./components/players";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Button, Form, FormControl, Nav } from "react-bootstrap";
 
+//react router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/players">Players</Nav.Link>
+              <Nav.Link href="/addplayer">Add</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar>
+          <br />
+          <Switch>
+            <Route path="/" component={Content} exact />
+            <Route path="/players" component={Players} exact />
+            <Route path="/addplayer" component={Players} exact />
+          </Switch>
+          {/* <Players></Players>
+          <Content></Content>
+          <Footer></Footer> */}
+        </div>
+      </Router>
+    );
+  } //end render
+} //end app
 export default App;
---
+++ src/components/players.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Players extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Players</h1>
+      </div>
+    );
+  }
+}
